51112192ae2cb511aa7f22a7b2a6a4a366a6b652
Tommaso <tommaso.lodesani@gmail.com>
2022-05-18 15:32:51 +0200

    creazione array oggetti

---
+++ script.js
@@ -0,0 +1,37 @@
+// creo arrey con oggetto lavoratori
+
+const arrayLavoratori = [
+
+    {
+        "Nome" : "Wayne Barnett",
+        "Ruolo" : "Founder & CEO",
+        "Foto" : "img/wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        "Nome" : "Angela Caroli",
+        "Ruolo" : "Chief Editor",
+        "Foto" : "img/angela-caroll-chief-editor.jpg"
+    },
+    {
+        "Nome" : "Walter Gordon",
+        "Ruolo" : "Office Manager",
+        "Foto" : "img/walter-gordon-office-manager.jpg"
+    },
+    {
+        "Nome" : "Angela Lopez",
+        "Ruolo" : "Social Media Manager",
+        "Foto" : "img/angela-lopez-social-media-manager.jpg"
+    },
+    {
+        "Nome" : "Scott Estrada",
+        "Ruolo" : "Developer",
+        "Foto" : "img/scott-estrada-developer.jpg"
+    },
+    {
+        "Nome" : "Barbara Ramos",
+        "Ruolo" : "Graphic Designer",
+        "Foto" : "img/barbara-ramos-graphic-designer.jpg"
+    }
+];
+
+console.log(arrayLavoratori);
